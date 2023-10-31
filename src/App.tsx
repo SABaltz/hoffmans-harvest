2ecf6c434d4ade8276ea402d1559e455db801502
sam <samuelabaltz@gmail.com>
2023-10-31 11:41:10 -0800

    Create NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./components/home-page/HomePage";
+import {ThemeProvider} from "@mui/material";
+import {plantTheme} from "./theme";
 
 function App() {
     return (
-        <HomePage></HomePage>
+        <ThemeProvider theme={plantTheme}>
+            <HomePage/>
+        </ThemeProvider>
     );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import {createTheme} from "@mui/material";
+
+export const plantTheme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#196904',
+        },
+        secondary: {
+            main: '#7a7a7a'
+        }
+
+    },
+    typography: {
+        fontFamily: 'poppins',
+    },
+})
