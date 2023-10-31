592e388ee57ed4a89422e70fd53dee103881cb41
sam <samuelabaltz@gmail.com>
2023-10-31 13:26:33 -0800

    Update Homepage with background photo

--- src/components/home-page/HomePage.tsx
+++ src/components/home-page/HomePage.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+// import terraniumWallpaper from "../../static/terranium-wallpaper.jxr";
+import terraniumWallpaper from "../../static/terranium-wallpaper.webp"
+import {Box} from "@mui/material";
 
 export default function HomePage() {
     return (
-        <div>home</div>
+        <Box
+            sx={{
+                backgroundImage: `url(${terraniumWallpaper})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                width: '100vw',
+                height: '100vh'
+            }}
+        ></Box>
     )
 }
