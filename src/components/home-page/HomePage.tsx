428d6d6382ffe53b4405365a2d4a831c201946fc
sam <samuelabaltz@gmail.com>
2023-11-01 07:52:03 -0800

    Create social media section on home page

--- src/components/home-page/HomePage.tsx
+++ src/components/home-page/HomePage.tsx
@@ -1,19 +1,50 @@
 import React from "react";
 // import terraniumWallpaper from "../../static/terranium-wallpaper.jxr";
 import terraniumWallpaper from "../../static/terranium-wallpaper.webp"
-import {Box} from "@mui/material";
+import {Box, Grid} from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 export default function HomePage() {
     return (
-        <Box
-            sx={{
-                backgroundImage: `url(${terraniumWallpaper})`,
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-                width: '100vw',
-                height: '100vh'
-            }}
-        ></Box>
+        <>
+            {/*--------------------------------------------------------------------------------*/}
+                <Box
+                    sx={{
+                        backgroundImage: `url(${terraniumWallpaper})`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover',
+                        width: '100vw',
+                        height: '100vh'
+                    }}
+                >
+
+                    <Typography
+                        variant="h3"
+                        color={'#eaeaea'}
+                        sx={{
+                            textAlign: 'center',
+                            paddingTop: '15rem'
+                        }}
+                    >
+                        Alaska's Premier Exotic Plant Nursery
+                    </Typography>
+                </Box>
+            {/*--------------------------------------------------------------------------------x*/}
+                <Box
+                    sx={{
+                        width: '100vw',
+                        height: '30vh'
+                    }}>
+                    <Grid container sx={{display: 'flex', justifyContent: 'center', alignContent: 'center', }}>
+                        <Grid item xs={3}>Social Media</Grid>
+                        <Grid item xs={3}>Insta</Grid>
+                        <Grid item xs={3}>Tic</Grid>
+                        <Grid item xs={3}>Facebook</Grid>
+
+                    </Grid>
+
+                </Box>
+        </>
     )
 }
