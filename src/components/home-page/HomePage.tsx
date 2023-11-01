d1a0c26c8526bc32f9113cc9e6cc2875ced35ba7
sam <samuelabaltz@gmail.com>
2023-11-01 10:44:45 -0800

    Create global-parameter folder

--- src/components/home-page/HomePage.tsx
+++ src/components/home-page/HomePage.tsx
@@ -1,54 +1,55 @@
 import React from "react";
-// import terraniumWallpaper from "../../static/terranium-wallpaper.jxr";
-import terraniumWallpaper from "../../static/terranium-wallpaper.webp"
+// import terrariumWallpaper from "../../static/terrarium-wallpaper.jxr";
+import terrariumWallpaper from "../../static/terranium-wallpaper.webp"
 import {Box, Grid} from "@mui/material";
 import Typography from "@mui/material/Typography";
+import {textColor} from '../../global-parameters/GlobalParams'
 
 export default function HomePage() {
     return (
         <>
-            {/*--------------------------------------------------------------------------------*/}
-                <Box
+            {/*-------------------------------------Wallpaper-------------------------------------------*/}
+            <Box
+                sx={{
+                    backgroundImage: `url(${terrariumWallpaper})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    width: '100vw',
+                    height: '100vh'
+                }}
+            >
+
+                <Typography
+                    variant="h3"
+                    color={textColor}
                     sx={{
-                        backgroundImage: `url(${terraniumWallpaper})`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundPosition: 'center',
-                        backgroundSize: 'cover',
-                        width: '100vw',
-                        height: '100vh'
+                        textAlign: 'center',
+                        paddingTop: '15rem'
                     }}
                 >
+                    Alaska's Premier Exotic Plant Nursery
+                </Typography>
+            </Box>
+            {/*----------------------------------Social Media----------------------------------------------x*/}
+            <Box
+                sx={{
+                    width: '100vw',
+                    height: '30vh',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignContent: 'center',
+                    alignItems: 'center',
+                }}>
+                <Grid container>
+                    <Grid item xs={12}>Social Media</Grid>
+                    <Grid item xs={3}>Instagram</Grid>
+                    <Grid item xs={3}>Tic</Grid>
+                    <Grid item xs={3}>Facebook</Grid>
 
-                    <Typography
-                        variant="h3"
-                        color={'#eaeaea'}
-                        sx={{
-                            textAlign: 'center',
-                            paddingTop: '15rem'
-                        }}
-                    >
-                        Alaska's Premier Exotic Plant Nursery
-                    </Typography>
-                </Box>
-            {/*--------------------------------------------------------------------------------x*/}
-                <Box
-                    sx={{
-                        width: '100vw',
-                        height: '30vh',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignContent: 'center',
-                        alignItems: 'center',
-                    }}>
-                    <Grid container>
-                        <Grid item xs={3}>Social Media</Grid>
-                        <Grid item xs={3}>Insta</Grid>
-                        <Grid item xs={3}>Tic</Grid>
-                        <Grid item xs={3}>Facebook</Grid>
+                </Grid>
 
-                    </Grid>
-
-                </Box>
+            </Box>
         </>
     )
 }
