d650547243e70771f854e06acf1176698592c298
sam <samuelabaltz@gmail.com>
2023-11-05 18:18:00 -0500

    Add redirect to remove blank url

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import HomePage from "./components/home-page/HomePage";
 import {ThemeProvider} from "@mui/material";
 import {plantTheme} from "./global-parameters/Theme";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import About from "./components/about/About";
 import Work from "./components/work/Work";
 import Contact from "./components/contact/Contact";
@@ -16,7 +16,7 @@ function App() {
             <NavBar/>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<><HomePage/></>}></Route>
+                    <Route path="/" element={<><Navigate to="/home" /></>} />
                     <Route path="/home" element={<><HomePage/></>}></Route>
                     <Route path="/about" element={<><About/></>}></Route>
                     <Route path="/work" element={<><Work/></>}></Route>
