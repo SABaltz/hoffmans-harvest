04aa4d4005241aabc9331cc9586d03b45264e045
sam <samuelabaltz@gmail.com>
2023-11-07 09:52:14 -0500

    Cleanup App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
-import './App.css';
-import HomePage from "./components/home-page/HomePage";
-import {ThemeProvider} from "@mui/material";
-import {plantTheme} from "./global-parameters/Theme";
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import About from "./components/about/About";
-import Work from "./components/work/Work";
-import Contact from "./components/contact/Contact";
-import NavBar from "./components/nav-bar/NavBar";
-import Footer from "./components/footer/Footer";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+import { plantTheme } from './global-parameters/Theme';
+import NavBar from './components/nav-bar/NavBar';
+import Footer from './components/footer/Footer';
+import HomePage from './components/home-page/HomePage';
+import About from './components/about/About';
+import Work from './components/work/Work';
+import Contact from './components/contact/Contact';
 
 function App() {
     return (
         <ThemeProvider theme={plantTheme}>
-            <NavBar/>
+            <NavBar />
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<><Navigate to="/home"/></>}/>
-                    <Route path="/home" element={<><HomePage/></>}></Route>
-                    <Route path="/about" element={<><About/></>}></Route>
-                    <Route path="/work" element={<><Work/></>}></Route>
-                    <Route path="/contact" element={<><Contact/></>}></Route>
+                    <Route path="/" element={<Navigate to="/home" />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/work" element={<Work />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Routes>
             </BrowserRouter>
-            <Footer/>
+            <Footer />
         </ThemeProvider>
     );
 }
