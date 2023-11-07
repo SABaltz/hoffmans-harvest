7b1bcc033c296e942205c6b0913ff6061fbee917
sam <samuelabaltz@gmail.com>
2023-11-07 12:09:52 -0500

    Update footer grid to center navigation stacks

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {navOptions, textColor} from '../../global-parameters/Parameters';
-import {AppBar, Link, Stack, Toolbar, Typography} from '@mui/material';
+import {AppBar, Grid, Link, Stack, Toolbar, Typography} from '@mui/material';
 import {centerVertHoriz} from '../../global-parameters/Styles';
 
 function Footer() {
@@ -9,25 +9,31 @@ function Footer() {
     return (
         <AppBar position="static">
             <Toolbar>
-                <Stack
-                    direction="row"
-                    spacing={2}
-                    flexGrow={1}
-                >
-                    {navOptions.map((option, index) => (
-                        <Typography key={index} variant="h6" component="div">
-                            <Link href={option} sx={{color: textColor}}>
-                                {option.toUpperCase()}
-                            </Link>
-                        </Typography>
-                    ))}
-                </Stack>
-                <Stack>
-                    <Typography sx={centerVertHoriz}>
-                        Copyright Hofffman's Harvest {currentYear}
-                    </Typography>
-                    <Typography sx={centerVertHoriz}>All Rights Reserved</Typography>
-                </Stack>
+                <Grid container>
+                    <Grid item xs={6} sx={{...centerVertHoriz}}>
+                        <Stack
+                            flexGrow={1}
+                            sx={{...centerVertHoriz}}
+                        >
+                            <Typography variant={'h5'}>Navigation</Typography>
+                            {navOptions.map((option, index) => (
+                                <Typography key={index} variant={'subtitle1'} component="div">
+                                    <Link href={option} sx={{color: textColor}}>
+                                        {option.toUpperCase()}
+                                    </Link>
+                                </Typography>
+                            ))}
+                        </Stack>
+                    </Grid>
+                    <Grid item xs={6} sx={{...centerVertHoriz}}>
+                        <Stack>
+                            <Typography sx={centerVertHoriz}>
+                                Copyright Hofffman's Harvest {currentYear}
+                            </Typography>
+                            <Typography sx={centerVertHoriz}>All Rights Reserved</Typography>
+                        </Stack>
+                    </Grid>
+                </Grid>
             </Toolbar>
         </AppBar>
     );
