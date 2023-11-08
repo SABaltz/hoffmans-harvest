1e89a5049940aa190b16e93b2ad1512ade6edb95
sam <samuelabaltz@gmail.com>
2023-11-07 20:22:00 -0500

    Add updating footer for portal login

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,12 @@
 import React, {useState} from 'react';
-import {navOptions, socialMediaLinks, textColor} from '../../global-parameters/Parameters';
-import {AppBar, Box, Grid, Link, Stack, Toolbar, Typography} from '@mui/material';
+import {
+    navOptions,
+    secondaryColor,
+    socialMediaLinks,
+    textColor,
+    textContrastColor
+} from '../../global-parameters/Parameters';
+import {AppBar, Box, Button, Grid, Link, Stack, Toolbar, Typography} from '@mui/material';
 import {centerVertHoriz} from '../../global-parameters/Styles';
 import {SocialIcon} from "react-social-icons";
 
@@ -25,61 +31,80 @@ function Footer() {
         <AppBar position="static">
             <Toolbar>
                 {showPortal ?
-                    <div>test1</div>
+                    // ----------------------------------------------------------------Portal Pass ----------------------------------------------
+                    <Grid container>
+                        <Grid item xs={6} sx={{...centerVertHoriz}}>
+                            <Stack>
+                                <Typography>Hello Mr. Hoffman</Typography>
+                                <Typography>Welcome to your website</Typography>
+                            </Stack>
+                        </Grid>
+                        <Grid item xs={6} sx={{...centerVertHoriz}}>
+                            <Button
+                                onClick={() => {
+                                    console.log('click')
+                                }}
+                                sx={{color: textContrastColor, backgroundColor: secondaryColor, display: 'block'}}>
+                                Go to Portal
+                            </Button>
+                        </Grid>
+                    </Grid>
+
                     :
-                    <div>test2</div>}
-                {/*----------------------------------------------------------------Regular Footer ----------------------------------------------*/}
-                <Grid container>
-                    <Grid item xs={4} sx={{...centerVertHoriz}}>
-                        <Stack
-                            flexGrow={1}
-                            sx={{...centerVertHoriz}}
-                        >
-                            <Typography variant={'h5'} onClick={() => {
-                                passSequence === .599 ?
-                                    resetPassSequence()
-                                    :
-                                    setPassSequence(0)
-                            }}>Navigation</Typography>
-                            {navOptions.map((option, index) => (
-                                <Typography key={index} variant={'subtitle1'} component="div">
-                                    <Link href={option} sx={{color: textColor}}>
-                                        {option.toUpperCase()}
-                                    </Link>
+                    // ----------------------------------------------------------------Regular Footer ----------------------------------------------
+                    <Grid container>
+                        <Grid item xs={4} sx={{...centerVertHoriz}}>
+                            <Stack
+                                flexGrow={1}
+                                sx={{...centerVertHoriz}}
+                            >
+                                <Typography variant={'h5'} onClick={() => {
+                                    passSequence === .599 ?
+                                        resetPassSequence()
+                                        :
+                                        setPassSequence(0)
+                                }}>Navigation</Typography>
+                                {navOptions.map((option, index) => (
+                                    <Typography key={index} variant={'subtitle1'} component="div">
+                                        <Link href={option} sx={{color: textColor}}>
+                                            {option.toUpperCase()}
+                                        </Link>
+                                    </Typography>
+                                ))}
+                            </Stack>
+                        </Grid>
+                        <Grid item xs={4} sx={{...centerVertHoriz}}>
+                            <Stack>
+                                <Typography onClick={() => {
+                                    setPassSequence(passSequence + .004)
+                                }} variant={'h5'} sx={centerVertHoriz}>
+                                    Follow Me
                                 </Typography>
-                            ))}
-                        </Stack>
+                                {socialMediaLinks.map((link, index) => (
+                                    <Box sx={{...centerVertHoriz, marginBottom: '.5rem'}}>
+                                        <SocialIcon style={{width: '2rem', height: '2rem'}} url={link.url}
+                                                    href={link.href}/>
+                                    </Box>
+                                ))}
+                            </Stack>
+                        </Grid>
+                        <Grid item xs={4} sx={{...centerVertHoriz}}>
+                            <Stack>
+                                <Typography sx={centerVertHoriz} onClick={() => {
+                                    setPassSequence(passSequence + .125)
+                                }}>
+                                    &copy;  Hofffman's Harvest {currentYear}
+                                </Typography>
+                                <Typography sx={centerVertHoriz} onClick={() => {
+                                    setPassSequence(passSequence + .235)
+                                }}>All Rights Reserved</Typography>
+                            </Stack>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={4} sx={{...centerVertHoriz}}>
-                        <Stack>
-                            <Typography onClick={() => {
-                                setPassSequence(passSequence + .004)
-                            }} variant={'h5'} sx={centerVertHoriz}>
-                                Follow Me
-                            </Typography>
-                            {socialMediaLinks.map((link, index) => (
-                                <Box sx={{...centerVertHoriz, marginBottom: '.5rem'}}>
-                                    <SocialIcon style={{width: '2rem', height: '2rem'}} url={link.url}
-                                                href={link.href}/>
-                                </Box>
-                            ))}
-                        </Stack>
-                    </Grid>
-                    <Grid item xs={4} sx={{...centerVertHoriz}}>
-                        <Stack>
-                            <Typography sx={centerVertHoriz} onClick={() => {
-                                setPassSequence(passSequence + .125)
-                            }}>
-                                &copy;  Hofffman's Harvest {currentYear}
-                            </Typography>
-                            <Typography sx={centerVertHoriz} onClick={() => {
-                                setPassSequence(passSequence + .235)
-                            }}>All Rights Reserved</Typography>
-                        </Stack>
-                    </Grid>
-                </Grid>
-                <Typography>{passSequence.toString()}</Typography>
-                {/*----------------------------------------------------------------Portal Pass ----------------------------------------------*/}
+
+
+                }
+
 
             </Toolbar>
         </AppBar>
