2a4912aa443126cd07dcd3d0bc1e7d09dcf936d3
sam <samuelabaltz@gmail.com>
2023-11-12 13:37:20 -0900

    Setup Contact.tsx content cards

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import plantPhone from "../../static/plant-phone.jpg";
-import {Box} from "@mui/material";
+import {Box, Card, CardContent, Grid, Typography} from "@mui/material";
+import {secondaryColor, textContrastColor} from "../../global-parameters/Parameters";
+import {centerVertHoriz} from "../../global-parameters/Styles";
+
 function Contact() {
     return (
         <Box>
@@ -15,7 +18,34 @@ function Contact() {
                     opacity: '80%'
                 }}
             >
-
+                <Grid container sx={{...centerVertHoriz}}>
+                    <Grid item xs={6} sx={{...centerVertHoriz}}>
+                        <Card sx={{maxWidth: '20rem', backgroundColor: secondaryColor}}>
+                            <CardContent>
+                                <Typography sx={{...centerVertHoriz}} color={textContrastColor} variant="h5"
+                                            component="div">
+                                    Call Us
+                                </Typography>
+                                <Typography variant="body2" color={textContrastColor}>
+                                    This is some content for the card. You can put any text or components here.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} sx={{...centerVertHoriz}}>
+                        <Card sx={{maxWidth: '20rem', backgroundColor: secondaryColor}}>
+                            <CardContent>
+                                <Typography sx={{...centerVertHoriz}} color={textContrastColor} variant="h5"
+                                            component="div">
+                                    Email Us
+                                </Typography>
+                                <Typography variant="body2" color={textContrastColor}>
+                                    This is some content for the card. You can put any text or components here.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
             </Box>
         </Box>
     )
