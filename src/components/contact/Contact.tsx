a593b3c53b35c64debd0eb8915f4cfcfddbe5caa
sam <samuelabaltz@gmail.com>
2023-11-12 14:01:12 -0900

    Add phone button to contact page

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,6 +3,7 @@ import plantPhone from "../../static/plant-phone.jpg";
 import {Box, Card, CardContent, Grid, Typography} from "@mui/material";
 import {secondaryColor, textContrastColor} from "../../global-parameters/Parameters";
 import {centerVertHoriz} from "../../global-parameters/Styles";
+import PhoneIcon from '@mui/icons-material/Phone';
 
 function Contact() {
     return (
@@ -18,34 +19,43 @@ function Contact() {
                     opacity: '80%'
                 }}
             >
-                <Grid container sx={{...centerVertHoriz}}>
-                    <Grid item xs={6} sx={{...centerVertHoriz}}>
-                        <Card sx={{maxWidth: '20rem', backgroundColor: secondaryColor}}>
-                            <CardContent>
-                                <Typography sx={{...centerVertHoriz}} color={textContrastColor} variant="h5"
-                                            component="div">
-                                    Call Us
-                                </Typography>
-                                <Typography variant="body2" color={textContrastColor}>
-                                    This is some content for the card. You can put any text or components here.
-                                </Typography>
-                            </CardContent>
-                        </Card>
+                <Box sx={{
+                    position: "absolute",
+                    top: "40%",
+                    width: "100%",
+                }}>
+                    <Grid container sx={{...centerVertHoriz}}>
+                        <Grid item xs={6} sx={{...centerVertHoriz}}>
+                            <Card sx={{maxWidth: '20rem', backgroundColor: secondaryColor}}>
+                                <CardContent>
+                                    <Typography sx={{...centerVertHoriz}} color={textContrastColor} variant="h5"
+                                                component="div">
+                                        Call Us
+                                    </Typography>
+
+                                    <Typography sx={{display: 'flex', alignItems: 'center'}} variant="body2"
+                                                color={textContrastColor}>
+                                        <PhoneIcon href={'tel:1-800-444-4444 '}
+                                                   sx={{paddingRight: '.5rem', cursor: 'pointer'}}/> (907) 999-9999
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} sx={{...centerVertHoriz}}>
+                            <Card sx={{maxWidth: '20rem', backgroundColor: secondaryColor}}>
+                                <CardContent>
+                                    <Typography sx={{...centerVertHoriz}} color={textContrastColor} variant="h5"
+                                                component="div">
+                                        Email Us
+                                    </Typography>
+                                    <Typography variant="body2" color={textContrastColor}>
+                                        This is some content for the card. You can put any text or components here.
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6} sx={{...centerVertHoriz}}>
-                        <Card sx={{maxWidth: '20rem', backgroundColor: secondaryColor}}>
-                            <CardContent>
-                                <Typography sx={{...centerVertHoriz}} color={textContrastColor} variant="h5"
-                                            component="div">
-                                    Email Us
-                                </Typography>
-                                <Typography variant="body2" color={textContrastColor}>
-                                    This is some content for the card. You can put any text or components here.
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                </Grid>
+                </Box>
             </Box>
         </Box>
     )
